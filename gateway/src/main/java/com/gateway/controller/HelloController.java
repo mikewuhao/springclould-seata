1c2d7c4d70f03db21a669f5973f402bfaba256b7
wangtiaochun <wangtiaochun@enn.cn>
2022-01-11 17:39:14 +0800

    分布式测试代码

---
+++ gateway/src/main/java/com/gateway/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.gateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello,my name is gateway";
+	}
+
+}
